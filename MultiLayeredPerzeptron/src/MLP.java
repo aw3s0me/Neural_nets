d12ae7637cb65dddd5059b615b8c382e7dad1794
Aleksander Korovin <akorovin00@gmail.com>
2015-11-05 23:54:58 +0100

    Almost done version

--- MultiLayeredPerzeptron/src/MLP.java
+++ MultiLayeredPerzeptron/src/MLP.java
@@ -14,13 +14,27 @@ public class MLP
         private int nOutput;
         private int nInput;
         private ITransferFunction transferFunction;
-        private double[][] input;
+        private double[] input;
         private double[][] weights;
         private double[][] dweights;
-        private double[][] output;
+        private double[] error;
+        private double[] output;
+        private double[] net;
+
+        public int getnOutput() {
+            return nOutput;
+        }
 
         public int getInputNumber() {
             return nInput;
+        }
+
+        public double[] getErrors() {
+            return error;
+        }
+
+        public double[][] getWeights() {
+            return weights;
         }
 
         public MLPLayer(int inputSize, int outputSize, ITransferFunction transferFunction, Random random) {
@@ -30,19 +44,14 @@ public class MLP
             initializeWeights(random);
         }
 
-        private double[][] applyBias(double[][] input, int nInput) {
-            double[][] newInput = new double[input.length][nInput + 1];
+        private double[] applyBias(double[] input, int nInput) {
+            double[] newInput = new double[nInput + 1];
 
             for (int i = 0; i < input.length; i++) {
-                for (int j = 0; j < input[i].length; j++) {
-                    newInput[i][j] = input[i][j];
-                }
+                newInput[i] = input[i];
             }
-
+            newInput[nInput] = 1;
             //Initialization of bias row (each element equals to 1
-            for (int i = 0; i < input.length; i++) {
-                newInput[i][nInput] = 1;
-            }
 
             return newInput;
         }
@@ -51,22 +60,23 @@ public class MLP
          * get output of MLPLayer
          * @param input
          */
-        public double[][] run(double[][] input) {
+        public double[] run(double[] input) {
             if (input == null)
             {
                 throw new IllegalArgumentException("input matrix cant be null");
             }
-            int nPattern = input.length;
-            double[][] newInput = applyBias(input, nInput);
-            double[][] multRes = Matrix.multiply(newInput, weights);
-
-//            output = new double[nPattern + 1][nOutput];
-            output = new double[nPattern][nOutput];
+            double[] newInput = applyBias(input, nInput);
+            net = new double[nOutput];
+            output = new double[nOutput];
+            this.input = newInput;
 
             for (int i = 0; i < output.length; i++) {
-                for (int j = 0; j < output[i].length; j++) {
-                    output[i][j] = transferFunction.calculate(multRes[i][j]);
+                output[i] = 0.0;
+                for (int j = 0; j < newInput.length; j++) {
+                    output[i] += weights[j][i] * newInput[j];
                 }
+                net[i] = output[i];
+                output[i] = transferFunction.calculate(output[i]);
             }
 
             return output;
@@ -82,6 +92,39 @@ public class MLP
                 }
             }
         }
+
+        private double[] getOutputError(double[] teacher) {
+            error = new double[nOutput];
+            for (int i = 0; i < error.length; i++) {
+                error[i] = (teacher[i] - output[i]) * transferFunction.calculateDerivative(net[i]);
+            }
+
+            return error;
+        }
+
+        private double[] getHiddenError(MLPLayer prevLayer) {
+            error = new double[nOutput];
+            double[] prevErrors = prevLayer.getErrors();
+            double[][] prevWeights = prevLayer.getWeights();
+            for (int i = 0; i < nOutput; i++) {
+                double sum = 0;
+                for (int j = 0; j < prevLayer.getnOutput(); j++) {
+                    sum += prevWeights[i][j] * prevErrors[j];
+                }
+                error[i] = sum * transferFunction.calculateDerivative(net[i]);
+            }
+
+            return error;
+        }
+
+        public void setNewWeights() {
+            for (int i = 0; i < nOutput; i++) {
+                for (int j = 0; j < nInput; j++) {
+                    weights[j][i] += LEARNING_RATE * error[i] * input[j];
+                }
+            }
+
+        }
     }
 
     private static final double LEARNING_RATE = 0.01;
@@ -90,6 +133,7 @@ public class MLP
     private static final double MIN_GEN_WEIGHT = -2;
     private static final double MAX_GEN_WEIGHT = 2;
     private static final int GEN_SEED = 2;
+    private int numOfPatternsGlobal;
     private final Random RANDOMIZER = new Random();
     //LAYERS
     private static final int HID_LAYER_NUM_1 = 6; //in case of 1-hidden-layer and 2-hidden layers.
@@ -159,21 +203,21 @@ public class MLP
             tempTeacherList.add(teacherRow);
         }
 
-        int nPatterns = tempInpList.size();
+        numOfPatternsGlobal = tempInpList.size();
         int nProperties = tempInpList.get(0).size();
         int nOutput = tempTeacherList.get(0).size();
 
-        fileInputs = new double[nPatterns][nProperties];
-        teacher = new double[nPatterns][nOutput];
+        fileInputs = new double[numOfPatternsGlobal][nProperties];
+        teacher = new double[numOfPatternsGlobal][nOutput];
 
-        for (int i = 0; i < nPatterns; i++) {
+        for (int i = 0; i < numOfPatternsGlobal; i++) {
             ArrayList<Double> list = tempInpList.get(i);
             for (int j = 0; j < nProperties; j++) {
                 fileInputs[i][j] = list.get(j);
             }
         }
 
-        for (int i = 0; i < nPatterns; i++) {
+        for (int i = 0; i < numOfPatternsGlobal; i++) {
             ArrayList<Double> list = tempTeacherList.get(i);
             for (int j = 0; j < nOutput; j++) {
                 teacher[i][j] = list.get(j);
@@ -211,32 +255,39 @@ public class MLP
     }
 
     private void iteration() {
-        double[][] output = fileInputs;
         for (int i = 0; i < ITERATIONS_NUM; i++) {
-            for (MLPLayer layer : layers) {
-                output = layer.run(output);
+            for (int j = 0; j < numOfPatternsGlobal; j++) {
+                double[] output = fileInputs[j];
+                for (MLPLayer layer : layers) {
+                    output = layer.run(output);
+                }
+                backPropagation(teacher[j]);
             }
-            backPropagation();
         }
     }
 
     /**
      * function to calculate gradient descent
      */
-    private void backPropagation() {
-//        double[][] transposedInputs = Matrix.transpose(fileInputs); //need to transpose input matrix, because it doesn't match dimensions
-//        double[][] errorResult = new double[nPatterns][nOutput]; //matrix to store intermediate result
-//
-//        for (int i = 0; i < nPatterns; i++) {
-//            for (int j = 0; j < nOutput; j++) {
-//                errorResult[i][j] = LEARNING_RATE *
-//                        (teacher[i][j] - sigmResult[i][j]) *
-//                        (1 - sigmResult[i][j]) *
-//                        sigmResult[i][j];
-//            }
-//        }
-//        //get new weights
-//        weights = Matrix.add(weights, Matrix.multiply(transposedInputs, errorResult));
+    private void backPropagation(double[] desiredOutput) {
+        double[] prevErrors = null;
+        for (int i = layers.size() - 1; i >= 0; i--) {
+            MLPLayer curLayer = layers.get(i);
+            if (i == layers.size() - 1) {
+                prevErrors = curLayer.getOutputError(desiredOutput);
+            }
+            else {
+                if (prevErrors == null) {
+                    throw new RuntimeException("Vector of errors from previous layer cannot be null");
+                }
+
+                prevErrors = curLayer.getHiddenError(layers.get(i+1));
+            }
+        }
+
+        for (MLPLayer mlpLayer : layers) {
+            mlpLayer.setNewWeights();
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -252,12 +303,19 @@ public class MLP
      */
     public interface ITransferFunction {
         double calculate(double val);
+        double calculateDerivative(double val);
     }
 
     public static class SigmoidFunction implements ITransferFunction {
         @Override
         public double calculate(double val) {
             return (1/( 1 + Math.pow(Math.E,(-1*val))));
+        }
+
+        @Override
+        public double calculateDerivative(double val) {
+            double funcVal = calculate(val);
+            return funcVal * (1- funcVal);
         }
     }
 
@@ -266,12 +324,23 @@ public class MLP
         public double calculate(double val) {
             return Math.tanh(val);
         }
+
+        @Override
+        public double calculateDerivative(double val) {
+            double funcVal = calculate(val);
+            return 1 - (funcVal * funcVal);
+        }
     }
 
     public static class IdentityFunction implements ITransferFunction {
         @Override
         public double calculate(double val) {
             return val;
+        }
+
+        @Override
+        public double calculateDerivative(double val) {
+            return 1;
         }
     }
 }
